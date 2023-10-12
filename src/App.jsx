a0f6ecf523218d8fa3ae83bdc5e92bdb1e6035c6
talawebhu <talaweb.hu@gmail.com>
2023-10-12 21:25:57 +0200

    Footer created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Newsletter from "./components/Newsletter";
 import Prices from "./components/Prices";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Newsletter />
       <Prices />
+      <Footer />
     </div>
   )
 }
